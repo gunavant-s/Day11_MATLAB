c067a0056298fcc19d174ec548aac42cbdbf2fe9
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2023-07-30 06:50:30 +0530

    Update isTherePythagoreanTriple.m

--- isTherePythagoreanTriple.m
+++ isTherePythagoreanTriple.m
@@ -1,5 +1,5 @@
 function flag = isTherePythagoreanTriple(a, b, c, d)
-    if (d^2 == b^2 + c^2 || d^2 == b^2 + a^2 || c^2 == b^2 + a^2)
+    if (d^2 == b^2 + c^2 || d^2 == b^2 + a^2 || d^2 == c^2 + a^2 || c^2 == b^2 + a^2)
         flag = 1;
     else
         flag = 0;
